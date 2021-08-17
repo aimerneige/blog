85b08168390d5e3fb7d2d453e42114e209f35372
Aimer Neige <aimer.neige.soft@gmail.com>
2021-08-17 09:10:16 +0800

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+hugo
+cd public
+git add -A
+msg="update site $(date)"
+if [ -n "$*" ]; then
+  msg="$*"
+fi
+git commit -m "$msg"
+git push origin master
