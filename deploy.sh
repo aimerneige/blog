dd0457984cf21ba340014d4db7f1f7dcd91ea032
Aimer Neige <aimer.neige.soft@gmail.com>
2024-12-15 16:13:48 +0800

    backup

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,39 @@
-#!/bin/sh
-
+#!/bin/bash
 set -e
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
-hugo
+
+
+info() {
+  printf "\033[0;32m$1\033[0m\n"
+}
+
+err() {
+  printf "\033[0;31m$1\033[0m\n"
+}
+
+cwd=$(pwd)
+if [[ ! -d "content" ]]; then
+    err "Run this under project root dir, currend dir: $cwd"
+    exit 1
+fi
+
+info "Deploying updates to GitHub..."
+
+if [[ -d ./public ]]; then
+  info "Deleting old public directory..."
+  rm -r public/
+fi
+
+info "Generating static files..."
+hugo --minify
+
+info "Pushing to github..."
 cd public
-git checkout master
+git init --initial-branch=main
+git remote add origin git@github.com:aimerneige/aimerneige.github.io.git
 git add -A
 msg="update site $(date)"
 if [ -n "$*" ]; then
   msg="$*"
 fi
-git commit -m "$msg"
-git push origin master
+git commit -am "$msg"
+git push -f origin main
